fe4afedf93d364748c7ee9cb36dafcc77cefd6ae
Elifarley C <elifarley@users.noreply.github.com>
2017-04-12 11:12:41 -0300

    shManaged: new function

--- src/com/orgecc/jpl/Base.groovy
+++ src/com/orgecc/jpl/Base.groovy
@@ -4,8 +4,21 @@ def shc(String commands) { sh(returnStdout: true, script: "${commands}").trim() 
 
 def getCommitId() { shc 'git rev-parse HEAD' }
 
-def dockerizeProject(String scriptID) {
-  configFileProvider([configFile(fileId: "org.jenkinsci.plugins.managedscripts.${scriptID}", targetLocation: '/tmp/$JOB_NAME/dockerize-project.sh', variable: 'script')]) {
+def shManaged(String scriptID) {
+  configFileProvider([configFile(fileId: "org.jenkinsci.plugins.managedscripts.${scriptID}", targetLocation: "/tmp/${scriptID}.sh", variable: 'script')]) {
     sh '. $script'
   }
 }
+
+def dockerizeProject() { sh """
+set -x
+mkdir -p .~/shell-lib
+curl -H 'Cache-Control: no-cache' -fsSL https://github.com/elifarley/shell-lib/archive/master.tar.gz | \
+tar -zx --strip-components 1 -C .~/shell-lib && \
+chmod +x .~/shell-lib/bin/* && PATH="$PWD/.~/shell-lib/bin:$PATH"
+
+test -e target || mkdir -p target 
+
+DEBUG=1 dockerize-project
+
+""" }
