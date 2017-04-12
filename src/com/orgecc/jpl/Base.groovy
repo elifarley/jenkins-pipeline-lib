549c65608a3c027c91bc7d4141d7936091709fb5
Elifarley C <elifarley@users.noreply.github.com>
2017-04-12 11:05:27 -0300

    dockerizeProject: new function

--- src/com/orgecc/jpl/Base.groovy
+++ src/com/orgecc/jpl/Base.groovy
@@ -3,3 +3,9 @@ package com.orgecc.jpl
 def shc(String commands) { sh(returnStdout: true, script: "${commands}").trim() }
 
 def getCommitId() { shc 'git rev-parse HEAD' }
+
+def dockerizeProject(String scriptID) {
+  configFileProvider([configFile(fileId: scriptID, targetLocation: '/tmp/$JOB_NAME/dockerize-project.sh', variable: 'script')]) {
+    sh '. $script'
+  }
+}
