e633e501d5b4655ac6803fad7af24e141c13f863
Elifarley C <elifarley@users.noreply.github.com>
2017-04-12 11:07:15 -0300

    Update Base.groovy

--- src/com/orgecc/jpl/Base.groovy
+++ src/com/orgecc/jpl/Base.groovy
@@ -5,7 +5,7 @@ def shc(String commands) { sh(returnStdout: true, script: "${commands}").trim() 
 def getCommitId() { shc 'git rev-parse HEAD' }
 
 def dockerizeProject(String scriptID) {
-  configFileProvider([configFile(fileId: scriptID, targetLocation: '/tmp/$JOB_NAME/dockerize-project.sh', variable: 'script')]) {
+  configFileProvider([configFile(fileId: "org.jenkinsci.plugins.managedscripts.${scriptID}", targetLocation: '/tmp/$JOB_NAME/dockerize-project.sh', variable: 'script')]) {
     sh '. $script'
   }
 }
