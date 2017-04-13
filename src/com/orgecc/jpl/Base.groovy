d9c04179041bd4cbb68ec56b046341bf4169e927
Elifarley C <elifarley@users.noreply.github.com>
2017-04-13 14:32:08 -0300

    Fix $PATH handling

--- src/com/orgecc/jpl/Base.groovy
+++ src/com/orgecc/jpl/Base.groovy
@@ -10,12 +10,12 @@ def shManaged(String scriptID) {
   }
 }
 
-def dockerizeProject() { sh """
+def dockerizeProject() { withEnv(["PATH=$WORKSPACE/.~/shell-lib/bin:${env.PATH}"]) { sh """
 set -x
 mkdir -p .~/shell-lib
 curl -H 'Cache-Control: no-cache' -fsSL https://github.com/elifarley/shell-lib/archive/master.tar.gz | \
 tar -zx --strip-components 1 -C .~/shell-lib && \
-chmod +x .~/shell-lib/bin/* && PATH="$PWD/.~/shell-lib/bin:$PATH"
+chmod +x .~/shell-lib/bin/*
 
 test -e target || mkdir -p target 
 
@@ -23,7 +23,7 @@ DEBUG=1 dockerize-project
 
 """
   archiveArtifacts artifacts: 'target/app.tgz'
-}
+}}
 
 /**
 Example:
@@ -36,14 +36,10 @@ withEnv(["GIT_COMMIT=${jplb.getCommitId()}", "DOCKER_REPO=my-repo"]) {
 */
 def dockerPush() { withEnv(["PATH=$WORKSPACE/.~/shell-lib/bin:${env.PATH}"]) { sh """
 set -x
-pwd
-
 mkdir -p .~/shell-lib
 curl -fsSL -H 'Cache-Control: no-cache' https://github.com/elifarley/shell-lib/archive/master.tar.gz | \
 tar -zx --strip-components 1 -C .~/shell-lib && \
 chmod +x .~/shell-lib/bin/*
-echo PATH: $PATH
-which jenkins-docker-push
 
 DEBUG=1 jenkins-docker-push \
 "${env.JOB_NAME}" "${env.BUILD_NUMBER}" "${env.GIT_COMMIT}" "${env.DOCKER_REPO}"
